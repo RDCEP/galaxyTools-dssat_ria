f67812299506cb98148908cd81ec25863c280254
Raffaele Montella <raffaele.montella@uniparthenope.it>
2016-06-02 16:09:38 +0000

    Added PACKAGE_BASE definition check

--- 1.0.0/env.sh
+++ 1.0.0/env.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+if [ -z "$PACKAGE_BASE" ];
+then
+  # For testing purposes
+  export PACKAGE_BASE="/mnt/galaxyTools/dssat_ria/1.0.0"
+  echo "Setting PACKAGE_BASE=$PACKAGE_BASE"
+fi
+
 export PATH=/mnt/galaxyTools/dssat_ria/1.0.0:$PATH
 export CLASSPATH=/mnt/galaxyTools/dssat_ria/1.0.0:$CLASSPATH
